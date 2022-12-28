9e382af353c77384fc5ebe1602eea2e8e8e39d34
AmbreshKumarSaini <92514207+AmbreshKumarSaini@users.noreply.github.com>
2022-12-29 01:14:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ var = Flask(__name__)
 @var.route('/',methods=["GET","POST"])
 def index():
 
-    url = "https://www.news18.com/tech"
+    url = "https://www.businesstoday.in/technology"
     req = requests.get(url)
-    soup= BeautifulSoup(req.content,"html.parser")
+    aas= BeautifulSoup(req.content,"html.parser")
 
-    outer_data = soup.find_all("div",class_="jsx-3621759782 blog_list_row",limit=5)
+    outer_data = aas.find_all("div",class_="widget-listing",limit=5)
     top5news=""
     for data in outer_data:
-        news = data.a.figure.figcaption.div.h4.string
+        news = data.div.h2.a.string
         top5news+="\u2022 "+news+"\n"
     return render_template("index.html" , news=top5news)
